2c809366c46accb8835d8b14a91c816b5b49ebdc
artur-nikolaichuk <59363088+artur-nikolaichuk@users.noreply.github.com>
2020-11-20 18:28:43 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+
+new WOW().init();
+
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
+const otmena = document.querySelector(".otmena");   
 cartButton.addEventListener("click", function(event){
     modal.classList.add("is-open");
 })
@@ -9,9 +13,14 @@ close.addEventListener("click", function(event){
     modal.classList.remove("is-open");
 })
 
+otmena.addEventListener("click", function(event){
+    modal.classList.remove("is-open");
+})
 
-new WOW().init();
+
 
 $(window).on("load",function(){
     $(".loader-container").fadeOut(3000);
 });
+
+
